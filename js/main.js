474fcc46a2349b7a4cddde31b91b89b0bc8f3d28
Kirill Gadomskii <gadomskyka@mail.ru>
2021-02-06 19:31:43 +0400

    Refactor createAnnouncement function, put constants at the beginnning

--- js/main.js
+++ js/main.js
@@ -1,27 +1,5 @@
 'use strict';
 
-const getRandomInteger = (min, max) => {
-  const error = 'Проверьте значения. Числа могут быть только положительными, значение max должно быть больше значения min';
-  const result = Math.floor(Math.random() * (max - min + 1) + min);
-
-  if (min < 0 || max < 0 || max <= min) {
-    throw new Error(error);
-  }
-
-  return result;
-};
-
-const getRandomFloat = (min, max, digits = 5) => {
-  const error = 'Проверьте значения. Числа могут быть только положительными, значение max должно быть больше значения min';
-  const result = (Math.random() * (max - min + 1) + min).toFixed(digits);
-
-  if (min < 0 || max < 0 || max <= min) {
-    throw new Error(error);
-  }
-
-  return +result;
-};
-
 const OFFER_TITLES = [
   'M-1 Tokyo Shimomaruko',
   'Studio Inn Nishi Shinjuku',
@@ -76,6 +54,28 @@ const OFFER_PHOTOS = [
 
 const ANNOUNCEMENT_QUANTITY = 10;
 
+const getRandomInteger = (min, max) => {
+  const error = 'Проверьте значения. Числа могут быть только положительными, значение max должно быть больше значения min';
+  const result = Math.floor(Math.random() * (max - min + 1) + min);
+
+  if (min < 0 || max < 0 || max <= min) {
+    throw new Error(error);
+  }
+
+  return result;
+};
+
+const getRandomFloat = (min, max, digits = 5) => {
+  const error = 'Проверьте значения. Числа могут быть только положительными, значение max должно быть больше значения min';
+  const result = (Math.random() * (max - min + 1) + min).toFixed(digits);
+
+  if (min < 0 || max < 0 || max <= min) {
+    throw new Error(error);
+  }
+
+  return +result;
+};
+
 const getRandomArrayElement = (elements) => {
   return elements[getRandomInteger(0, elements.length - 1)];
 };
@@ -88,56 +88,40 @@ const getUniqueArrayElements = (elements) => {
   const randomArray = new Array(getRandomArrayLength(elements)).fill(null).map(() => {
     return getRandomArrayElement(elements);
   });
+
   return [...new Set(randomArray)];
 };
 
-const getAuthorInfo = () => {
-  return {
-    avatar: `img/avatars/user0${getRandomInteger(1, 8)}.png`,
-  };
-};
-
-let coordinateX = 0;
-let coordinateY = 0;
-
-const getCoordinates = () => {
-  coordinateX = getRandomFloat(35.65000,35.70000);
-  coordinateY = getRandomFloat(139.70000,139.80000);
-
-  return {
-    x: coordinateX,
-    y: coordinateY,
-  };
-};
-
-const getOffer = () => {
-  return {
-    title: getRandomArrayElement(OFFER_TITLES),
-    address: coordinateX + ', ' + coordinateY,
-    price: getRandomInteger(9500, 30000),
-    type: getRandomArrayElement(OFFER_TYPES),
-    rooms: getRandomInteger(1, 5),
-    guests: getRandomInteger(1, 3),
-    checkin: getRandomArrayElement(OFFER_CHECKINS),
-    checkout: getRandomArrayElement(OFFER_CHECKOUTS),
-    features: getUniqueArrayElements(OFFER_FEATURES),
-    description: getRandomArrayElement(OFFER_DESCRIPTIONS),
-    photos: getUniqueArrayElements(OFFER_PHOTOS),
-  };
-};
-
 const createAnnouncement = () => {
+  const coordinateX = getRandomFloat(35.65000,35.70000);
+  const coordinateY = getRandomFloat(139.70000,139.80000);
+
   return {
-    author: getAuthorInfo(),
-    location: getCoordinates(),
-    offer: getOffer(),
+    author: {
+      avatar: `img/avatars/user0${getRandomInteger(1, 8)}.png`,
+    },
+    offer: {
+      title: getRandomArrayElement(OFFER_TITLES),
+      address: coordinateX + ', ' + coordinateY,
+      price: getRandomInteger(9500, 30000),
+      type: getRandomArrayElement(OFFER_TYPES),
+      rooms: getRandomInteger(1, 5),
+      guests: getRandomInteger(1, 3),
+      checkin: getRandomArrayElement(OFFER_CHECKINS),
+      checkout: getRandomArrayElement(OFFER_CHECKOUTS),
+      features: getUniqueArrayElements(OFFER_FEATURES),
+      description: getRandomArrayElement(OFFER_DESCRIPTIONS),
+      photos: getUniqueArrayElements(OFFER_PHOTOS),
+    },
+    location: {
+      x: coordinateX,
+      y: coordinateY,
+    },
   };
 };
 
 const createSeveralAnnouncements = () => {
-  return new Array(ANNOUNCEMENT_QUANTITY).fill(null).map(() => {
-    return createAnnouncement();
-  });
+  return new Array(ANNOUNCEMENT_QUANTITY).fill(null).map(createAnnouncement);
 };
 
 createSeveralAnnouncements();
